e8e26067f3b9b3049582301a25dcaa01069b6b2a
Viktor <viktorplane.sonie@gmail.com>
2019-07-09 22:00:16 +0200

    add login tests

--- test/init.test.ts
+++ test/init.test.ts
@@ -1,13 +1,14 @@
-import {register} from "../src/blockchain/UserService";
-import {getANumber, makeKeyPair} from "./helper";
+import { register, login } from "../src/blockchain/UserService";
+import { getANumber, makeKeyPair } from "./helper";
 
 
 import * as bip39 from "bip39";
+import { User } from "../src/types";
 
 jest.setTimeout(30000);
 
 
-describe('Registers users', () => {
+describe('User Authentication', () => {
 
     const admin = {
         name: "admin",
@@ -22,12 +23,28 @@ describe('Registers users', () => {
     };
 
 
-    it("registers one user", async () => {
+    it("register user " + user01.name, async () => {
+        expect.assertions(1);
+        
         await expect(register(user01.name, user01.password, user01.mnemonic)).resolves.toBe(null);
+        console.log("Registered", user01.name, " with mnemonic", user01.mnemonic);
     });
 
-    it("registers one admin", async () => {
+    it("register and login admin user", async () => {
+        expect.assertions(1);
+
         await expect(register(admin.name, admin.password, admin.mnemonic)).resolves.toBe(null);
-        console.log(admin.mnemonic);
-    })
+        console.log("Registered", admin.name, "with mnemonic", admin.mnemonic);
+    });
+
+    it("login user " + user01.name, async () => {
+        expect.assertions(3);
+
+        const user: User = await login(user01.name, user01.password, user01.mnemonic);
+        console.log("Logged in", user);
+
+        expect(user).toBeDefined();
+        expect(user.name).toBe(user01.name);
+        expect(user.seed).toBeDefined();
+    });
 });
