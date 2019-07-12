f1839db5123ed33ff588cea7b06dd627f6b5d765
Viktor <viktorplane.sonie@gmail.com>
2019-07-12 17:26:37 +0200

    add more tets coverage

--- test/util.test.ts
+++ test/util.test.ts
@@ -4,7 +4,7 @@ jest.setTimeout(30000);
 
 describe("Sorting by frequently tests", () => {
 
-    it("list of strings", async done => {
+    it("list of strings", async () => {
         const fruits: string[] = [ "apple", "orange", "orange", "banana" ];
         const sortedFruits: string[] = sortByFrequency(fruits);
 
@@ -12,62 +12,53 @@ describe("Sorting by frequently tests", () => {
         expect(sortedFruits[0]).toBe("orange");
         expect(sortedFruits[1]).toBe("apple");
         expect(sortedFruits[2]).toBe("banana");
-
-        done();
     });
 
 });
 
 describe("Sorting by frequently tests", () => {
 
-    it("time ago to readable months", async done => {
+    it("time ago to readable months", async () => {
         const monthInMillis: number = 2629743000;
         expect(timeAgoReadable(Date.now() - monthInMillis)).toBe("1 month ago");
         expect(timeAgoReadable(Date.now() - (monthInMillis * 2))).toBe("2 months ago");
-        done();
     });
 
-    it("time ago to readable days", async done => {
+    it("time ago to readable days", async () => {
         const dayInMillis: number = 86400000;
         expect(timeAgoReadable(Date.now() - dayInMillis)).toBe("1 day ago");
         expect(timeAgoReadable(Date.now() - (dayInMillis * 2))).toBe("2 days ago");
-        done();
     });
 
-    it("time ago to readable hours", async done => {
+    it("time ago to readable hours", async () => {
         const hourInMillis: number = 3600000;
         expect(timeAgoReadable(Date.now() - hourInMillis)).toBe("1 hour ago");
         expect(timeAgoReadable(Date.now() - (hourInMillis * 2))).toBe("2 hours ago");
-        done();
     });
 
-    it("time ago to readable hours", async done => {
+    it("time ago to readable hours", async () => {
         const minuteInMillis: number = 60000;
         expect(timeAgoReadable(Date.now())).toBe("0 minutes ago");
         expect(timeAgoReadable(Date.now() - minuteInMillis)).toBe("1 minute ago");
         expect(timeAgoReadable(Date.now() - (minuteInMillis * 2))).toBe("2 minutes ago");
-        done();
     });
 
 });
 
 describe("message should be sliced tests", () => {
 
-    it("message longer than 300 chars should be sliced", async done => {
+    it("message longer than 300 chars should be sliced", async () => {
         expect(needsToBeSliced("a".repeat(350))).toBe(true);
-        done();
     });
 
-    it("message shorter than 300 chars should not be sliced", async done => {
+    it("message shorter than 300 chars should not be sliced", async () => {
         expect(needsToBeSliced("a".repeat(250))).toBe(false);
-        done();
     });
 
-    it("message with 300 chars should not be sliced", async done => {
+    it("message with 300 chars should not be sliced", async () => {
         const message: string = "a".repeat(300);
         expect(message.length).toBe(300);
         expect(needsToBeSliced("a".repeat(300))).toBe(false);
-        done();
     });
 
 });
