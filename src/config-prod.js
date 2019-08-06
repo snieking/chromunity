c3a951ba6b2429b38d83429b5e37e2384598aa20
Viktor Plane <viktorplane.sonie@gmail.com>
2019-08-06 19:30:35 +0200

    test s3 deploy

---
+++ src/config-prod.js
@@ -0,0 +1,7 @@
+// @ts-ignore
+const config = {};
+
+config.nodeApiUrl = "http://ec2-34-244-206-159.eu-west-1.compute.amazonaws.com:7740";
+config.blockchainRID = "78967baa4768cbcef11c508326ffb13a956689fcb6dc3ba17f4b895cbb1577a3";
+
+module.exports = config;
