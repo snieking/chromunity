434709abc765e4f05aeda4bb23c50e5c47484de4
Viktor Plane <viktorplane.sonie@gmail.com>
2019-08-07 20:14:08 +0200

    Fix image compression for avatar

--- src/components/user/Settings/AvatarChanger/AvatarChanger.tsx
+++ src/components/user/Settings/AvatarChanger/AvatarChanger.tsx
@@ -16,22 +16,59 @@ class AvatarChanger extends React.Component<AvatarChangerProps, AvatarChangerSta
 
     constructor(props: AvatarChangerProps) {
         super(props);
-        this.state = {src: ""}
+        this.state = {src: ""};
 
         this.onCrop = this.onCrop.bind(this);
         this.onBeforeFileLoad = this.onBeforeFileLoad.bind(this);
     }
 
+    compressImage(base64: string) {
+        const canvas = document.createElement('canvas');
+        const img = document.createElement('img');
+
+        return new Promise((resolve, reject) => {
+            img.onload = function () {
+                let width = img.width;
+                let height = img.height;
+                const maxHeight = 128;
+                const maxWidth = 128;
+
+                if (width > height) {
+                    if (width > maxWidth) {
+                        height = Math.round((height *= maxWidth / width));
+                        width = maxWidth;
+                    }
+                } else {
+                    if (height > maxHeight) {
+                        width = Math.round((width *= maxHeight / height));
+                        height = maxHeight;
+                    }
+                }
+                canvas.width = width;
+                canvas.height = height;
+
+                const ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0, width, height);
+
+                resolve(canvas.toDataURL('image/jpeg', 0.7))
+            };
+            img.onerror = function (err) {
+                reject(err);
+            };
+
+            img.src = base64;
+        })
+    }
+
     onCrop(preview: string) {
-        this.setState({src: preview})
-        this.props.updateFunction(preview);
+        this.compressImage(preview).then(img => {
+            this.setState({src: preview});
+            this.props.updateFunction(preview);
+        });
     }
 
     onBeforeFileLoad(elem: { target: { files: { size: number; }[]; value: string; }; }) {
-        if (elem.target.files[0].size > 71680) {
-            alert("File is too big!");
-            elem.target.value = "";
-        }
+
     }
 
     render() {
