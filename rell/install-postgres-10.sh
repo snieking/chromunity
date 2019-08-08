4eb651bdc8a2c7c53c215717b70553e7e0274584
Viktor Plane <viktorplane.sonie@gmail.com>
2019-08-08 08:58:27 +0200

    add pubkey required for xenial builds

--- rell/install-postgres-10.sh
+++ rell/install-postgres-10.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+echo "adding pubkey required for apt-get update"
+sudo apt-key adv —keyserver keyserver.ubuntu.com —recv-keys 762E3157
+
 echo "Installing Postgres 10"
 sudo service postgresql stop
 sudo apt-get remove -q 'postgresql-*'
