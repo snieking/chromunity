26ee502007bb3a3b195a749fb2b7572cbf5e548f
snieking <viktorplane.sonie@gmail.com>
2019-08-30 13:13:12 +0200

    Switch url to new domain name

--- src/config-dev.js
+++ src/config-dev.js
@@ -4,7 +4,7 @@ const config = {};
 config.nodeApiUrl = "http://ec2-34-244-206-159.eu-west-1.compute.amazonaws.com:7740";
 config.blockchainRID = "88967baa4768cbcef11c508326ffb13a956689fcb6dc3ba17f4b895cbb1577a3";
 
-config.chromunityUrl = "http://dev.chromunity.com.s3-website-eu-west-1.amazonaws.com";
+config.chromunityUrl = "https://dev.chromunity.com";
 config.vaultUrl = "https://wallet-v2.chromia.dev";
 
 module.exports = config;
