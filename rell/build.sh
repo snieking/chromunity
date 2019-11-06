5d2e2dd369601c5136e010b1fbfe3f58b9544c2c
Viktor Plane <viktorplane.sonie@gmail.com>
2019-11-06 19:34:47 +0100

    upgrade to rell 0.10.0

--- rell/build.sh
+++ rell/build.sh
@@ -5,20 +5,10 @@
 
 set -eu
 
-CHAIN_DIRS=()
-CHAIN_DIRS[1]=src/chain1
+rm -rf blockchain
+mkdir blockchain
 
-CONF=dev
-
-rm -rf config/blockchains
-mkdir config/blockchains
-
-for i in "${!CHAIN_DIRS[@]}"; do
-   CHAIN_ID=$i
-   CHAIN_DIR=${CHAIN_DIRS[$i]}
-   echo "Compiling $CHAIN_DIR as chain $CHAIN_ID"
-   mkdir config/blockchains/$CHAIN_ID
-   postchain-node/rellcfg.sh --template $CHAIN_DIR/config/$CONF/config.template.xml $CHAIN_DIR/rell/main.rell config/blockchains/$CHAIN_ID/0.xml
-   cp $CHAIN_DIR/config/$CONF/brid.txt config/blockchains/$CHAIN_ID/brid.txt
-done
-
+echo "Compiling Chromunity blockchain"
+postchain-node/rellcfg.sh --template config/config.template.xml -d src/ "" blockchain/blockchain.xml
+cp config/brid.txt blockchain/brid.txt
+echo "Successfully compiled Chromunity blockchain"
