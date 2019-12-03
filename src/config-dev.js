e3c490c93becb6ad7874e39462937ff37cfa1435
Viktor Plane <viktorplane.sonie@gmail.com>
2019-12-03 21:32:24 +0100

    use old brid for already deployed branches

--- src/config-dev.js
+++ src/config-dev.js
@@ -2,7 +2,7 @@
 const config = {};
 
 config.nodeApiUrl = "https://cg7tt9fhch.execute-api.eu-central-1.amazonaws.com/dev/";
-config.blockchainRID = process.env.REACT_APP_BRID;
+config.blockchainRID = "88967baa4768cbcef11c508326ffb13a956689fcb6dc3ba17f4b895cbb1577a3";
 
 config.chromunityUrl = "https://dev.chromunity.com";
 config.vaultUrl = "https://wallet-v2.chromia.dev";
