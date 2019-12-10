b497f1401b1ab02186407e04275dc67264ad49cf
Viktor Plane <viktorplane.sonie@gmail.com>
2019-12-10 16:44:50 +0100

    remove storage of postchain and download with script

--- rell/download-binaries.sh
+++ rell/download-binaries.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-PACKAGE=rellr-0.10.0-v30-3.0.0-dist.tar.gz
+PACKAGE=rellr-0.10.1-dev-3.0.1-dist.tar.gz
 
 echo "Downloading ${PACKAGE}"
 curl -o ${PACKAGE} -L http://dist.chromia.dev/rellr/${PACKAGE}
