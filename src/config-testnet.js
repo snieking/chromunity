7508e9bdc05e2090fccfac215fe48a3604c4dcd7
Viktor Plane <viktorplane.sonie@gmail.com>
2020-02-04 18:58:53 +0100

    Use github actions

---
+++ src/config-testnet.js
@@ -0,0 +1,26 @@
+// @ts-ignore
+module.exports = {
+  blockchain: {
+    nodeApiUrl: "https://cg7tt9fhch.execute-api.eu-central-1.amazonaws.com/dev/",
+    rid: "7ED3148E3F74230CC4CD90CDC33B55C2DC50CD878A238E61BB0CC28A84C10068"
+  },
+  vault: {
+    url: "https://wallet-v2.chromia.dev",
+    callbackBaseUrl: "https://dev.chromunity.com"
+  },
+  sentry: {
+    dsn: "https://9fcd3766732c4665b89f08e2d58b286f@sentry.io/1870353",
+    environment: "testnet"
+  },
+  matomo: {
+    url: "https://matomo.chromia.dev/",
+    siteId: 3,
+    trackErrors: true,
+    jsFileName: "js/",
+    phpFilename: "js/"
+  },
+  topBar: {
+    message: "TestNet"
+  },
+  test: true
+};
