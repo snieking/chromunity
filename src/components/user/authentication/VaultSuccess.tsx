82725ca8f410119b214dc97e7654aaacab84b7c5
Viktor Plane <viktorplane.sonie@gmail.com>
2020-02-24 19:50:31 +0100

    Update success page

--- src/components/user/authentication/VaultSuccess.tsx
+++ src/components/user/authentication/VaultSuccess.tsx
@@ -6,6 +6,9 @@ import { LinearProgress } from "@material-ui/core";
 import { vaultSuccess, resetLoginState } from "../redux/accountActions";
 import { ApplicationState } from "../../../store";
 import { ChromunityUser } from "../../../types";
+import ChromiaPageHeader from "../../common/ChromiaPageHeader";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 
 interface MatchParams {
   username: string;
@@ -18,21 +21,37 @@ interface Props extends RouteComponentProps<MatchParams> {
   error: string;
 }
 
+const useStyles = makeStyles({
+  contentWrapper: {
+    textAlign: "center"
+  }
+});
+
 const VaultSuccess: React.FunctionComponent<Props> = props => {
-  const query = parse(props.location.search);
-  const rawTx: string = query.rawTx as string;
-  const username: string = props.match.params.username;
+  const classes = useStyles();
 
   useEffect(() => {
+    const query = parse(props.location.search);
+    const rawTx: string = query.rawTx as string;
+    const username: string = props.match.params.username;
+
     if (rawTx && username) {
       props.vaultSuccess(rawTx, username);
     }
-  }, [rawTx, username]);
+  }, [props]);
 
   if (props.error != null) {
     return <Redirect to={"/user/login"} />;
   } else if (props.user == null) {
-    return <LinearProgress variant="query" />;
+    return (
+      <>
+        <LinearProgress variant="query" />
+        <Container maxWidth="md" className={classes.contentWrapper}>
+          <ChromiaPageHeader text={"Almost there!"} />
+          <p>Please give us a second to authenticate you...</p>
+        </Container>
+      </>
+    );
   } else {
     return <Redirect to={"/"} />;
   }
