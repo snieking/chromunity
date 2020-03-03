b98d7bb27cae3d0313947c89443b3ff5e4021220
Viktor Plane <viktorplane.sonie@gmail.com>
2020-03-03 19:02:09 +0100

    Upgrade rell version

--- rell/download-binaries.sh
+++ rell/download-binaries.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-PACKAGE=rellr-0.10.1-dev-3.0.1-dist.tar.gz
+PACKAGE=rellr-0.10.2-3.1.3-dist.tar.gz
 
 echo "Downloading ${PACKAGE}"
 curl -o ${PACKAGE} -L http://dist.chromia.dev/rellr/${PACKAGE}
