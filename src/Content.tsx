10d17a6eb35c3aab1d4110a6da1998caeffbcffe
Viktor Plane <viktorplane.sonie@gmail.com>
2020-03-19 19:22:11 +0100

    Remove transition groups

--- src/Content.tsx
+++ src/Content.tsx
@@ -9,8 +9,6 @@ import Representatives from "./components/governing/representatives/Representati
 import Election from "./components/governing/election/Election";
 import { GovLog } from "./components/governing/GovLog";
 import FullTopic from "./components/topic/FullTopic";
-import CSSTransition from "react-transition-group/CSSTransition";
-import TransitionGroup from "react-transition-group/TransitionGroup";
 import { Route, RouteComponentProps, Switch, withRouter } from "react-router";
 import TopicWall from "./components/walls/TopicWall";
 import WalletLogin from "./components/user/authentication/WalletLogin";
@@ -23,31 +21,27 @@ import Reports from "./components/governing/reports/Reports";
 const Content: React.FunctionComponent<RouteComponentProps> = ({ location }) => {
   return (
     <div className="content">
-      <TransitionGroup>
-        <CSSTransition timeout={500} classNames="animate" key={location.key}>
-          <Switch>
-            <Route exact path="/" component={() => <TopicWall key="topic-wall" type="all" />} />
-            <Route path="/followings" component={() => <TopicWall type="userFollowings" />} />
-            <Route path="/channels" component={() => <TopicWall type="tagFollowings" />} />
-            <Route path="/vault/success" component={VaultSuccess} />
-            <Route path="/vault/cancel" component={VaultCancel} />
-            <Route path="/user/login" component={WalletLogin} />
-            <Route path="/user/logout" component={Logout} />
-            <Route path="/user/settings" component={Settings} />
-            <Route path="/u/:userId" component={UserWall} />
-            <Route path="/notifications/:userId" component={UserNotifications} />
-            <Route path="/c/:channel" component={ChannelWall} />
-            <Route path="/gov/representatives" component={Representatives} />
-            <Route path="/gov/election" component={Election} />
-            <Route path="/gov/vote/:candidate" component={CandidateElectionVoteLink} />
-            <Route path="/gov/log" component={GovLog} />
-            <Route path="/gov/reports" component={Reports} />
-            <Route path="/t/:id" component={FullTopic} />
-            <Route path="/chat" component={ChatPage} />
-            <Route path="/error" component={ErrorPage} />
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
+      <Switch>
+        <Route exact path="/" component={() => <TopicWall key="topic-wall" type="all" />} />
+        <Route path="/followings" component={() => <TopicWall type="userFollowings" />} />
+        <Route path="/channels" component={() => <TopicWall type="tagFollowings" />} />
+        <Route path="/vault/success" component={VaultSuccess} />
+        <Route path="/vault/cancel" component={VaultCancel} />
+        <Route path="/user/login" component={WalletLogin} />
+        <Route path="/user/logout" component={Logout} />
+        <Route path="/user/settings" component={Settings} />
+        <Route path="/u/:userId" component={UserWall} />
+        <Route path="/notifications/:userId" component={UserNotifications} />
+        <Route path="/c/:channel" component={ChannelWall} />
+        <Route path="/gov/representatives" component={Representatives} />
+        <Route path="/gov/election" component={Election} />
+        <Route path="/gov/vote/:candidate" component={CandidateElectionVoteLink} />
+        <Route path="/gov/log" component={GovLog} />
+        <Route path="/gov/reports" component={Reports} />
+        <Route path="/t/:id" component={FullTopic} />
+        <Route path="/chat" component={ChatPage} />
+        <Route path="/error" component={ErrorPage} />
+      </Switch>
     </div>
   );
 };
