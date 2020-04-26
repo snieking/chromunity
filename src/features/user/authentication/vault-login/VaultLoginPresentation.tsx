d3eea52856e7c655936c934b8a6c7978215fe6dc
Viktor Plane <viktorplane.sonie@gmail.com>
2020-04-26 09:32:56 +0200

    Fix graphics path

--- src/features/user/authentication/vault-login/VaultLoginPresentation.tsx
+++ src/features/user/authentication/vault-login/VaultLoginPresentation.tsx
@@ -5,8 +5,8 @@ import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
-import { ReactComponent as LeftShapes } from "../../../shared/graphics/left-shapes.svg";
-import { ReactComponent as RightShapes } from "../../../shared/graphics/right-shapes.svg";
+import { ReactComponent as LeftShapes } from "../../../../shared/graphics/left-shapes.svg";
+import { ReactComponent as RightShapes } from "../../../../shared/graphics/right-shapes.svg";
 import { AuthenticationStep } from "../../redux/accountTypes";
 
 const useStyles = makeStyles(theme =>
