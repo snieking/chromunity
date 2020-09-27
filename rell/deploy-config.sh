087edf71359ad14781b10f4b2b645301538192fc
Viktor Plane <viktorplane.sonie@gmail.com>
2020-09-27 09:00:31 +0200

    Update deploy script

--- rell/deploy-config.sh
+++ rell/deploy-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-  
+
 set -e
 
-BLOCK_HEIGHT=$(sudo -u postgres psql -qtAX -c 'SET search_path to blockchain; SELECT block_height FROM blocks ORDER BY block_height DESC LIMIT 1;' chromunity)
+BLOCK_HEIGHT=$(sudo -u postgres psql -qtAX -c 'SET search_path to blockchain; SELECT block_height FROM "c1.blocks" ORDER BY block_height DESC LIMIT 1;' chromunity)
 DEPLOY_HEIGHT=$((BLOCK_HEIGHT + 10))
 
 echo "Current block height is $BLOCK_HEIGHT, new config will be deployed at block height $DEPLOY_HEIGHT"
